7f4dc913b7b346671438cbea61b81dea2df7c052
yadav.ks <yadav.ks@infospica.com>
2025-12-24 15:20:47 +0530

    defaul instance added

---
+++ src/main/java/com/example/demo/controller/InstanceController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/version")
+public class InstanceController {
+
+    @Value("${application.default.instance}")
+    private String appInstance;
+
+    @GetMapping("/instance") // Maps the root URL
+    public String hello() {
+
+        return "Hello You are using"+appInstance+"instance"; // This text will appear in the browser
+    }
+}
